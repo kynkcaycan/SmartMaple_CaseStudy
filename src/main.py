9c7e04e5c64831e45de79f89b9e43dd2d5597002
kynkcaycan <105385269+kynkcaycan@users.noreply.github.com>
2025-05-12 22:38:08 +0300

    commit3

--- src/main.py
+++ src/main.py
@@ -8,7 +8,13 @@ If you have any questions in mind you can connect to me directly via info@smart-
 """
 
 """
-İlk olarak, gerekli bağımlılıkları içeren veritabanı bağlantısı başlatılır (init_db() fonksiyonu ile). Ardından, kamp alanları verisini çekmek için scrape_campgrounds() fonksiyonu belirli aralıklarla çalıştırılacak şekilde zamanlanır. Zamanlama, schedule modülü kullanılarak her 3 dakikada bir bu işin yapılması sağlanır. Uygulama çalıştırıldığında, veritabanı bağlantısı kurulur ve scraper'ın işleyişine başlamak için bir iş başlatılır. Ayrıca, scraper sürekli olarak belirtilen zaman diliminde çalışmaya devam eder. 
+İlk olarak, gerekli bağımlılıkları içeren veritabanı bağlantısı başlatılır (init_db() fonksiyonu ile).
+
+Ardından, kamp alanları verisini çekmek için scrape_campgrounds() fonksiyonu belirli aralıklarla çalıştırılacak şekilde zamanlanır. Zamanlama, schedule modülü kullanılarak her 3 dakikada bir bu işin yapılması sağlanır.
+
+Uygulama çalıştırıldığında, veritabanı bağlantısı kurulur ve scraper'ın işleyişine başlamak için bir iş başlatılır. 
+
+Ayrıca, scraper sürekli olarak belirtilen zaman diliminde çalışmaya devam eder. 
 """
 from models import Base
 from scraper import scrape_campgrounds
